b19b45087403673ce3b43c135077ed8820787655
Mike Cutalo <mcutalo88@gmail.com>
2019-09-04 08:56:56 -0700

    refactor name to cslice

--- src/cslice.rs
+++ src/cslice.rs
@@ -4,7 +4,7 @@ use clap::{Arg, App};
 use std::io::{self, Read};
 
 fn main() -> io::Result<()> {
-    let slicecol = App::new("slicecol")
+    let cslice = App::new("cslice")
                     .version("1.0")
                     .author("Mike C. <mcutalo88@gmail.com>")
                     .arg(Arg::with_name("column")
@@ -17,8 +17,8 @@ fn main() -> io::Result<()> {
                     .get_matches();
 
 
-    let col_arg = slicecol.value_of("column").unwrap_or("0");
-    let skip_arg = slicecol.value_of("skip").unwrap_or("0");
+    let col_arg = cslice.value_of("column").unwrap_or("0");
+    let skip_arg = cslice.value_of("skip").unwrap_or("0");
 
     let mut buffer = String::new();
     let stdin = io::stdin();
